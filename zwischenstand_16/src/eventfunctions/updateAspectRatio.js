c3ec011407d789782ea8437b5f4f48853fb9a28b
neuhoefer <j.a.neuhoefer@web.de>
2020-04-24 15:50:22 +0200

    Change.

---
+++ zwischenstand_16/src/eventfunctions/updateAspectRatio.js
@@ -0,0 +1,6 @@
+function updateAspectRatio(event) {
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
